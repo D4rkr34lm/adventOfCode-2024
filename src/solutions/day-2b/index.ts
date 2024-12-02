b338f2e3c4095560b45a39bd887f08725da2791b
D4rkr34lm <darth.m.frohn@gmail.com>
2024-12-02 22:08:59 +0100

    Solved 2b

---
+++ src/solutions/day-2b/index.ts
@@ -0,0 +1,35 @@
+import _ from "lodash";
+import { slideDualWindow } from "../../utils/slidingWindows";
+
+function solve(data: string): string {
+  const solution = _.chain(data)
+    .split("\n")
+    .map(report => report.split(" ").map(n => parseInt(n)))
+    .filter((report) => !_.isEqual(report, [NaN]))
+    .map(report => {
+      console.log(report)
+      const [first, second] = _.take(report, 2)
+
+      const isDescending = first > second;
+      const comparator = (a: number, b: number) => isDescending ? a > b : a < b;
+
+      let isStable = 2;
+
+      slideDualWindow(report, ([ a, b ]) => {
+        if(!comparator(a,b) || Math.abs(a - b) > 3){
+          isStable--;
+        }
+      })
+
+      return isStable;
+    })
+    .filter((isSafe) => isSafe > 0)
+    .value()
+    .length;
+
+    console.log(solution)
+
+    return solution.toString()
+}
+
+export default solve;
